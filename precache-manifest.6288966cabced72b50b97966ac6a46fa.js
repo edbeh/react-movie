75bcaa802caec46a99c5d6ee82807d1e36fb0e7a
edbeh89 <edbeh89@gmail.com>
2019-08-09 14:38:29 +0800

    Updates

---
+++ precache-manifest.6288966cabced72b50b97966ac6a46fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4607a65e4ce2f391fa4d2b2d422bc543",
+    "url": "/react-movie/index.html"
+  },
+  {
+    "revision": "cd38b1dbd2382ba61b9e",
+    "url": "/react-movie/static/css/main.dcfd379e.chunk.css"
+  },
+  {
+    "revision": "ef7b0852847cba54cf1c",
+    "url": "/react-movie/static/js/2.cdfc99b3.chunk.js"
+  },
+  {
+    "revision": "cd38b1dbd2382ba61b9e",
+    "url": "/react-movie/static/js/main.9ec351f8.chunk.js"
+  },
+  {
+    "revision": "5de7cd1febd31cdee4f7",
+    "url": "/react-movie/static/js/runtime~main.b4244ab5.js"
+  }
+]);
