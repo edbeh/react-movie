4ee1bf23454afb72bf25f8daf2f42c6d443a9a17
edward <edbeh89@gmail.com>
2021-05-29 14:02:26 +0800

    Updates

--- precache-manifest.c0b7542f4b71cd2c31ca5decc101ddfb.js
+++ precache-manifest.c0b7542f4b71cd2c31ca5decc101ddfb.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eb83a961adba16984e41144bc1a3450a",
+    "revision": "4cc4917c2d604e1a50be7a78f2f9faa2",
     "url": "/react-movie/index.html"
   },
   {
-    "revision": "0ebe07b417e6ee3526ff",
+    "revision": "3aa3cb71b3448f29c5f7",
     "url": "/react-movie/static/css/main.52aa479c.chunk.css"
   },
   {
-    "revision": "41091bace13125c82207",
-    "url": "/react-movie/static/js/2.8d2a291f.chunk.js"
+    "revision": "0ff87c9eb479aef82904",
+    "url": "/react-movie/static/js/2.6fe7fd58.chunk.js"
   },
   {
     "revision": "6deb9ada2d2a90964ebad279404033bd",
-    "url": "/react-movie/static/js/2.8d2a291f.chunk.js.LICENSE.txt"
+    "url": "/react-movie/static/js/2.6fe7fd58.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0ebe07b417e6ee3526ff",
-    "url": "/react-movie/static/js/main.47833d50.chunk.js"
+    "revision": "3aa3cb71b3448f29c5f7",
+    "url": "/react-movie/static/js/main.5d4ec999.chunk.js"
   },
   {
     "revision": "70ff08b51119de3fac99",
